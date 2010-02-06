b614712addaeb448f60eb3b8fb6c87cda7bfa52d
Ayende Rahien <Ayende@ayende.com>
2010-02-06 02:50:31 +0200

    Adding support for rude disposal for the queue storage

--- Rhino.Queues/Storage/QueueStorage.cs
+++ Rhino.Queues/Storage/QueueStorage.cs
@@ -1,52 +1,53 @@
 using System;
 using System.IO;
 using System.Runtime.ConstrainedExecution;
+using log4net;
 using Microsoft.Isam.Esent.Interop;
-using System.Diagnostics;
 
 namespace Rhino.Queues.Storage
 {
-    public class QueueStorage : CriticalFinalizerObject, IDisposable
-    {
-        private JET_INSTANCE instance;
-        private readonly string database;
-        private readonly string path;
-    	private ColumnsInformation columnsInformation;
+	public class QueueStorage : CriticalFinalizerObject, IDisposable
+	{
+		private readonly ILog log = LogManager.GetLogger(typeof(QueueStorage));
+		private JET_INSTANCE instance;
+		private readonly string database;
+		private readonly string path;
+		private ColumnsInformation columnsInformation;
 
-        public Guid Id { get; private set; }
+		public Guid Id { get; private set; }
 
-        public QueueStorage(string database)
-        {
-            this.database = database;
-            path = database;
-            if (Path.IsPathRooted(database) == false)
-                path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, database);
-            this.database = Path.Combine(path, Path.GetFileName(database));
-            Api.JetCreateInstance(out instance, database + Guid.NewGuid());
-        }
+		public QueueStorage(string database)
+		{
+			this.database = database;
+			path = database;
+			if (Path.IsPathRooted(database) == false)
+				path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, database);
+			this.database = Path.Combine(path, Path.GetFileName(database));
+			Api.JetCreateInstance(out instance, database + Guid.NewGuid());
+		}
 
-        public void Initialize()
-        {
-            ConfigureInstance(instance);
-            try
-            {
-                Api.JetInit(ref instance);
+		public void Initialize()
+		{
+			ConfigureInstance(instance);
+			try
+			{
+				Api.JetInit(ref instance);
 
-                EnsureDatabaseIsCreatedAndAttachToDatabase();
+				EnsureDatabaseIsCreatedAndAttachToDatabase();
 
-                SetIdFromDb();
+				SetIdFromDb();
 
-            	LoadColumnInformation();
-            }
-            catch (Exception e)
-            {
-                Dispose();
-                throw new InvalidOperationException("Could not open queue: " + database, e);
-            }
-        }
+				LoadColumnInformation();
+			}
+			catch (Exception e)
+			{
+				Dispose();
+				throw new InvalidOperationException("Could not open queue: " + database, e);
+			}
+		}
 
-    	private void LoadColumnInformation()
-    	{
+		private void LoadColumnInformation()
+		{
 			columnsInformation = new ColumnsInformation();
 			instance.WithDatabase(database, (session, dbid) =>
 			{
@@ -79,139 +80,164 @@ namespace Rhino.Queues.Storage
 					columnsInformation.RecveivedMsgsColumns = Api.GetColumnDictionary(session, table);
 				}
 			});
-    	}
+		}
 
-    	private void ConfigureInstance(JET_INSTANCE jetInstance)
-        {
-            new InstanceParameters(jetInstance)
-            {
-                CircularLog = true,
-                Recovery = true,
-                CreatePathIfNotExist = true,
-                TempDirectory = Path.Combine(path, "temp"),
-                SystemDirectory = Path.Combine(path, "system"),
-                LogFileDirectory = Path.Combine(path, "logs"),
-                MaxVerPages = 8192,
-                MaxTemporaryTables = 8192
-            };
-        }
+		private void ConfigureInstance(JET_INSTANCE jetInstance)
+		{
+			new InstanceParameters(jetInstance)
+			{
+				CircularLog = true,
+				Recovery = true,
+				CreatePathIfNotExist = true,
+				TempDirectory = Path.Combine(path, "temp"),
+				SystemDirectory = Path.Combine(path, "system"),
+				LogFileDirectory = Path.Combine(path, "logs"),
+				MaxVerPages = 8192,
+				MaxTemporaryTables = 8192
+			};
+		}
 
-        private void SetIdFromDb()
-        {
-            try
-            {
-                instance.WithDatabase(database, (session, dbid) =>
-                {
-                    using (var details = new Table(session, dbid, "details", OpenTableGrbit.ReadOnly))
-                    {
-                        Api.JetMove(session, details, JET_Move.First, MoveGrbit.None);
-                        var columnids = Api.GetColumnDictionary(session, details);
-                        var column = Api.RetrieveColumn(session, details, columnids["id"]);
-                        Id = new Guid(column);
-                        var schemaVersion = Api.RetrieveColumnAsString(session, details, columnids["schema_version"]);
-                        if (schemaVersion != SchemaCreator.SchemaVersion)
-                            throw new InvalidOperationException("The version on disk (" + schemaVersion + ") is different that the version supported by this library: " + SchemaCreator.SchemaVersion + Environment.NewLine +
-                                                                "You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.");
-                    }
-                });
-            }
-            catch (Exception e)
-            {
-                throw new InvalidOperationException("Could not read db details from disk. It is likely that there is a version difference between the library and the db on the disk." + Environment.NewLine +
-                                                    "You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.", e);
-            }
-        }
+		private void SetIdFromDb()
+		{
+			try
+			{
+				instance.WithDatabase(database, (session, dbid) =>
+				{
+					using (var details = new Table(session, dbid, "details", OpenTableGrbit.ReadOnly))
+					{
+						Api.JetMove(session, details, JET_Move.First, MoveGrbit.None);
+						var columnids = Api.GetColumnDictionary(session, details);
+						var column = Api.RetrieveColumn(session, details, columnids["id"]);
+						Id = new Guid(column);
+						var schemaVersion = Api.RetrieveColumnAsString(session, details, columnids["schema_version"]);
+						if (schemaVersion != SchemaCreator.SchemaVersion)
+							throw new InvalidOperationException("The version on disk (" + schemaVersion + ") is different that the version supported by this library: " + SchemaCreator.SchemaVersion + Environment.NewLine +
+																"You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.");
+					}
+				});
+			}
+			catch (Exception e)
+			{
+				throw new InvalidOperationException("Could not read db details from disk. It is likely that there is a version difference between the library and the db on the disk." + Environment.NewLine +
+													"You need to migrate the disk version to the library version, alternatively, if the data isn't important, you can delete the file and it will be re-created (with no data) with the library version.", e);
+			}
+		}
 
-        private void EnsureDatabaseIsCreatedAndAttachToDatabase()
-        {
-            using (var session = new Session(instance))
-            {
-                try
-                {
-                    Api.JetAttachDatabase(session, database, AttachDatabaseGrbit.None);
-                    return;
-                }
-                catch (EsentErrorException e)
-                {
-                    if (e.Error == JET_err.DatabaseDirtyShutdown)
-                    {
-                        try
-                        {
-                            using (var recoverInstance = new Instance("Recovery instance for: " + database))
-                            {
-                                recoverInstance.Init();
-                                using (var recoverSession = new Session(recoverInstance))
-                                {
-                                    ConfigureInstance(recoverInstance.JetInstance);
-                                    Api.JetAttachDatabase(recoverSession, database,
-                                                          AttachDatabaseGrbit.DeleteCorruptIndexes);
-                                    Api.JetDetachDatabase(recoverSession, database);
-                                }
-                            }
-                        }
-                        catch (Exception)
-                        {
-                        }
+		private void EnsureDatabaseIsCreatedAndAttachToDatabase()
+		{
+			using (var session = new Session(instance))
+			{
+				try
+				{
+					Api.JetAttachDatabase(session, database, AttachDatabaseGrbit.None);
+					return;
+				}
+				catch (EsentErrorException e)
+				{
+					if (e.Error == JET_err.DatabaseDirtyShutdown)
+					{
+						try
+						{
+							using (var recoverInstance = new Instance("Recovery instance for: " + database))
+							{
+								recoverInstance.Init();
+								using (var recoverSession = new Session(recoverInstance))
+								{
+									ConfigureInstance(recoverInstance.JetInstance);
+									Api.JetAttachDatabase(recoverSession, database,
+														  AttachDatabaseGrbit.DeleteCorruptIndexes);
+									Api.JetDetachDatabase(recoverSession, database);
+								}
+							}
+						}
+						catch (Exception)
+						{
+						}
 
-                        Api.JetAttachDatabase(session, database, AttachDatabaseGrbit.None);
-                        return;
-                    }
-                    if (e.Error != JET_err.FileNotFound)
-                        throw;
-                }
+						Api.JetAttachDatabase(session, database, AttachDatabaseGrbit.None);
+						return;
+					}
+					if (e.Error != JET_err.FileNotFound)
+						throw;
+				}
 
-                new SchemaCreator(session).Create(database);
-                Api.JetAttachDatabase(session, database, AttachDatabaseGrbit.None);
-            }
-        }
+				new SchemaCreator(session).Create(database);
+				Api.JetAttachDatabase(session, database, AttachDatabaseGrbit.None);
+			}
+		}
 
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-            try
-            {
-                Api.JetTerm(instance);
-            }
-            catch (Exception)
-            {
-                //nothing much to do if I can't close it
-            }
-        }
+		public void Dispose()
+		{
+			log.Debug("Disposing queue storage");
+			try
+			{
+				Api.JetTerm2(instance, TermGrbit.Complete);
+				GC.SuppressFinalize(this);
+			}
+			catch (Exception e)
+			{
+				log.Error("Could not dispose of queue storage properly", e);
+				throw;
+			}
+		}
 
-        ~QueueStorage()
-        {
-            try
-            {
-                Trace.WriteLine(
-                    "Disposing esent resources from finalizer! You should call QueueStorage.Dispose() instead!");
-                Api.JetTerm(instance);
-            }
-            catch (Exception exception)
-            {
-                try
-                {
-                    Trace.WriteLine("Failed to dispose esent instance from finalizer because: " + exception);
-                }
-                catch
-                {
-                }
-            }
-        }
+		public void DisposeRudely()
+		{
+			log.Debug("Rudely disposing queue storage");
+			try
+			{
+				Api.JetTerm2(instance, TermGrbit.Abrupt);
+				GC.SuppressFinalize(this);
+			}
+			catch (Exception e)
+			{
+				log.Error("Could not dispose of queue storage properly", e);
+				throw;
+			}
+		}
 
-        public void Global(Action<GlobalActions> action)
-        {
-            using (var qa = new GlobalActions(instance, columnsInformation, database, Id))
-            {
-                action(qa);
-            }
-        }
 
-        public void Send(Action<SenderActions> action)
-        {
+		~QueueStorage()
+		{
+			try
+			{
+				log.Warn("Disposing esent resources from finalizer! You should call QueueStorage.Dispose() instead!");
+				Api.JetTerm2(instance, TermGrbit.Complete);
+			}
+			catch (Exception exception)
+			{
+				try
+				{
+					log.Error("Failed to dispose esent instance from finalizer, trying abrupt termination.", exception);
+					try
+					{
+						Api.JetTerm2(instance, TermGrbit.Abrupt);
+					}
+					catch (Exception e)
+					{
+						log.Error("Could not dispose esent instance abruptly", e);
+					}
+				}
+				catch
+				{
+				}
+			}
+		}
+
+		public void Global(Action<GlobalActions> action)
+		{
+			using (var qa = new GlobalActions(instance, columnsInformation, database, Id))
+			{
+				action(qa);
+			}
+		}
+
+		public void Send(Action<SenderActions> action)
+		{
 			using (var qa = new SenderActions(instance, columnsInformation, database, Id))
-            {
-                action(qa);
-            }
-        }
-    }
+			{
+				action(qa);
+			}
+		}
+	}
 }
