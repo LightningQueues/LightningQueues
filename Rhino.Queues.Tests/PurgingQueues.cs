fb17194468e9356d95e80829a9443a4b659d68ea
Ryan Hauert <ryan.hauert@extendhealth.com>
2013-03-15 15:51:17 -0600

    Fixed a test to work with .net 3.5

--- Rhino.Queues.Tests/PurgingQueues.cs
+++ Rhino.Queues.Tests/PurgingQueues.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Transactions;
 using Xunit;
 
@@ -27,25 +27,56 @@ namespace Rhino.Queues.Tests
             queueManager.Start();
 
             // Seed the queue with historical messages to be purged
-            Parallel.For(0, 1000, new ParallelOptions { MaxDegreeOfParallelism = 8 }, i => SendMessages());
+            QueueMessagesThreaded(1000);
+            //Parallel.For(0, 1000, new ParallelOptions { MaxDegreeOfParallelism = 8 }, i => SendMessages());
 
             queueManager.WaitForAllMessagesToBeSent();
 
             // Try to purge while still sending new messages.
-            var purgeTask = Task.Factory.StartNew(() =>
+            var waitHandle = new ManualResetEvent(false);
+            ThreadPool.QueueUserWorkItem(_ =>
             {
                 queueManager.PurgeOldData();
                 Console.WriteLine("Finished purging data");
+                waitHandle.Set();
             });
-            Parallel.For(0, 10000, new ParallelOptions { MaxDegreeOfParallelism = 8 }, i => SendMessages());
+            QueueMessagesThreaded(10000);
+            //var purgeTask = Task.Factory.StartNew(() =>
+            //{
+            //    queueManager.PurgeOldData();
+            //    Console.WriteLine("Finished purging data");
+            //});
+            //Parallel.For(0, 10000, new ParallelOptions { MaxDegreeOfParallelism = 8 }, i => SendMessages());
 
-            purgeTask.Wait();
+            waitHandle.WaitOne();
+            //purgeTask.Wait();
             queueManager.WaitForAllMessagesToBeSent();
 
             queueManager.PurgeOldData();
 
             Assert.Equal(queueManager.Configuration.NumberOfMessagesToKeepInOutgoingHistory,
                 queueManager.GetAllSentMessages().Length);
+        }
+
+        private void QueueMessagesThreaded(int iterations)
+        {
+            const int threadCount = 8;
+            int iterationsPerThread = iterations / threadCount;
+            var threads = new List<Thread>();
+            for (int i = 0; i < threadCount; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    for (int j = 0; j < iterationsPerThread; j++)
+                    {
+                        SendMessages();
+                    }
+                });
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            threads.ForEach(x => x.Join());
         }
 
         private void SendMessages()
