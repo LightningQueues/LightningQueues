f59888d096f8e0268df6cce1f1065528ca2b32c4
Corey Kaylor <corey@kaylors.net>
2013-07-25 15:15:03 -0600

    Using git tag for version now

--- default.ps1
+++ default.ps1
@@ -6,7 +6,7 @@ properties {
   $40_build_dir = "$build_dir\4.0\"
   $35_build_dir = "$build_dir\3.5\"
   $sln_file = "$base_dir\Rhino.Queues.sln" 
-  $version = "2.1.0.0"
+  $version = Get-Version-From-Git-Tag
   $config = "Release"
   $tools_dir = "$base_dir\Tools"
   $release_dir = "$base_dir\Release"
