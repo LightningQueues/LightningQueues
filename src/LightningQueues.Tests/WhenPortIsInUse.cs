6cb96672e158929621b966fc92a66f6523ceaeb2
Corey Kaylor <corey@kaylors.net>
2013-10-11 09:41:25 -0600

    Added test for error message

--- src/LightningQueues.Tests/WhenPortIsInUse.cs
+++ src/LightningQueues.Tests/WhenPortIsInUse.cs
@@ -1,4 +1,6 @@
-﻿using FubuTestingSupport;
+﻿using System;
+using System.Net;
+using FubuTestingSupport;
 using LightningQueues.Exceptions;
 using NUnit.Framework;
 
@@ -14,6 +16,13 @@ namespace LightningQueues.Tests
             var two = ObjectMother.QueueManager("test2");
             one.Start();
             typeof(EndpointInUseException).ShouldBeThrownBy(two.Start);
+        }
+
+        [Test]
+        public void ExceptionMessageIsCorrect()
+        {
+            var exception = new EndpointInUseException(new IPEndPoint(IPAddress.Loopback, 2200), new Exception());
+            exception.Message.ShouldEqual("The endpoint 127.0.0.1:2200 is already in use");
         }
     }
 }
