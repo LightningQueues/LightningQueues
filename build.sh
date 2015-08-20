b146c64eba998933be89fcc46ee795d763e8abca
Corey Kaylor <corey@kaylors.net>
2015-08-20 12:33:33 -0600

    Updated build script to compile lmdb on linux

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf artifacts
+
 if ! type dnvm > /dev/null 2>&1; then
     curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
 fi
@@ -9,5 +10,17 @@ dnvm alias default 1.0.0-beta6
 dnvm use default
 dnu restore
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-dnx ./tests/LightningQueues.Tests test
+
+if [ "$(uname)" == "Darwin" ]; then
+    dnx ./tests/LightningQueues.Tests test        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    git clone https://github.com/LMDB/lmdb.git
+    type make >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'make' for lmdb native lib compile.  Aborting."; exit 1; }
+    type gcc >/dev/null 2>&1 || { echo >&2 "Can't find dependency 'gcc' for lmdb native lib compile.  Aborting."; exit 1; }
+    cd mdb/libraries/liblmdb/
+    make
+    cd ../../../
+    LD_LIBRARY_PATH=./mdb/libraries/liblmdb/:$LD_LIBRARY_PATH dnx ./tests/LightningDB.Tests test
+fi
+
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
