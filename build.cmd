2d1f4e271141924bc6c134bdc5d67cb1321bd879
Corey Kaylor <corey@kaylors.net>
2015-08-20 09:45:23 -0600

    Added build script for Windows that also creates nuget artifacts

---
+++ build.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+pushd %~dp0
+
+setlocal EnableDelayedExpansion 
+where dnvm
+if %ERRORLEVEL% neq 0 (
+    @powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}"
+    set PATH=!PATH!;!userprofile!\.dnx\bin
+    set DNX_HOME=!USERPROFILE!\.dnx
+    goto install
+)
+
+:install
+call dnvm install 1.0.0-beta6
+call dnvm use 1.0.0-beta6
+rem set the runtime path because the above commands set \.dnx<space>\runtimes
+set PATH=!USERPROFILE!\.dnx\runtimes\dnx-clr-win-x86.1.0.0-beta6\bin;!PATH!
+
+call dnu restore
+if %errorlevel% neq 0 exit /b %errorlevel%
+call dnx .\tests\LightningQueues.Tests test
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Packing LightningQueues Nuget Version %LQ_NUGET_VERSION%
+call dnu pack src\LightningQueues --configuration Release --out artifacts
+call dnu pack src\LightningQueues.Storage.LMDB --configuration Release --out artifacts
+
+popd
