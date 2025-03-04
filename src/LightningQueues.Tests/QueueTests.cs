13e037c2d34f3bd973a7a09c34f8cab07c11ac4e
Corey Kaylor <corey@kaylors.net>
2025-03-04 15:27:39 -0500

    Adding delay for verifying message deletion

--- src/LightningQueues.Tests/QueueTests.cs
+++ src/LightningQueues.Tests/QueueTests.cs
@@ -150,6 +150,7 @@ public class QueueTests : TestBase
             payloads.ShouldContain("payload1");
             payloads.ShouldContain("payload2");
             payloads.ShouldContain("payload3");
+            await DeterministicDelay(TimeSpan.FromSeconds(1), token);
             
             // Verify the message store shows they were all sent
             var store = (LmdbMessageStore)queue.Store;
